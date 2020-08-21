5256955bdb6ba5eec923cf7de5d8007c50242283
Petar Scepanovic <hiborija@gmail.com>
2020-08-21 14:07:26 +0200

    fixed user selector bug

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+
 import { userReducer } from './user/userReducer';
 import { cartReducer } from './cart/cartReducer';
 
